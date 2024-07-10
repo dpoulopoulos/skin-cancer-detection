b3ad17a9b0502e8fdc7a9b7f5876d24942d12257
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-07-10 10:57:06 +0000

    data: Add the dataset and data module scrips
    Add two Python scripts that:
    * Define a PyTorch Dataset using the ISIC 2024 competition files.
    * Define a Lightning DataModule using the PyTorch Dataset.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

---
+++ dataset.py
@@ -0,0 +1,35 @@
+from io import BytesIO
+
+import h5py
+
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class ISICDataset(Dataset):
+    def __init__(self, hdf5_file, isic_ids, targets=None, transform=None):
+        self.hdf5_file = hdf5_file
+        self.isic_ids = isic_ids
+        self.targets = targets
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.isic_ids)
+    
+    def __getitem__(self, idx):
+        isic_id = self.isic_ids[idx]
+
+        # convert byte array to PIL image
+        image = Image.open(BytesIO(self.hdf5_file[isic_id][()]))
+        
+        if self.transform:
+            image = self.transform(image)
+
+        if self.targets is not None:
+            target = self.targets[idx]
+            return image, target
+        else:
+            return image
+
+    def close(self):
+        self.hdf5.close()

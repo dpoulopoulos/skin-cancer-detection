a95208a1259fcb986ea4a001fcc090f820bac8fa
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-07-18 10:17:33 +0300

    dockerfiles: Enable model checkpointing
    Define a model checkpoint callback that retains the top 2 models based
    on validation loss.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- dockerfiles/dist_training/src/main.py
+++ dockerfiles/dist_training/src/main.py
@@ -4,6 +4,7 @@ import torch
 import lightning as L
 
 from lightning.pytorch.strategies import DDPStrategy
+from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.plugins.environments import KubeflowEnvironment
 
 from model import LitEfficientNet
@@ -19,12 +20,22 @@ def main():
 
     environment = KubeflowEnvironment()
     strategy = DDPStrategy(cluster_environment=environment)
+
+    # saves top-K checkpoints based on "val_loss" metric
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=2,
+        monitor="valid_loss",
+        mode="min",
+        dirpath="/logs/checkpoints/",
+        filename="sample-isic-{epoch:02d}-{val_loss:.5f}")
+
     trainer = L.Trainer(
         max_epochs=4,
         precision="bf16-mixed",
         accelerator="gpu",
         num_nodes=2,
-        strategy=strategy)
+        strategy=strategy,
+        callbacks=[checkpoint_callback])
 
     trainer.fit(model, datamodule=datamodule)
 
